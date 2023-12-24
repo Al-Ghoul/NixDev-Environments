4cea19d8f2364d002bf3afcb54dc458fd424bd9f
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-24 22:59:13 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
   description = "A collection of development environments and templates";
 
-  outputs = { self}:
+  outputs = { self }:
   {
     templates = {
       react-native = {
         path = ./ReactNative;
         description = "A React Native development environment with emulator included";
       };
+
       cpp = {
         path = ./Cpp;
         description = "A Cpp development environment with boost libraries included";
