01f046954727b5568796ac9efee020ce6dab4984
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-30 04:58:35 +0200

    Update

--- templates/ReactNative/flake.nix
+++ templates/ReactNative/flake.nix
@@ -39,7 +39,7 @@
           jdk17
           watchman # Required for 'metro' for better performance 
           androidComposition.platform-tools # Expose platform tools (aka adb & other executables)
-          nixgl.nixGLIntel # Fixes GPU usage issue; GLIntel only supports AMD & Intel GPUs, for Nvidia you might wanna use other options.
+          pkgs.nixgl.nixGLIntel # Fixes GPU usage issue; GLIntel only supports AMD & Intel GPUs, for Nvidia you might wanna use other options.
           nodePackages.eas-cli # Expo's EAS Update CLI
         ];
       
