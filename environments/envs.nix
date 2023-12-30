ec3872b3164a56001f9357111b808f829bed813a
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-30 04:47:26 +0200

    Update

--- environments/envs.nix
+++ environments/envs.nix
@@ -5,6 +5,7 @@
     name = "Cpp development shell environment";
 
     packages = with pkgs; [ boost clang-tools vscode-extensions.vadimcn.vscode-lldb.adapter];
+    packages = with pkgs; [ boost clang-tools vscode-extensions.vadimcn.vscode-lldb.adapter ];
 
     shellHook = ''
       echo "The cpp-dev env was set successfully";
